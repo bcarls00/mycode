29cbfcff460e44395783d160e404d49d834d56e9
bcarls00 <bacarlson2200@gmail.com>
2023-08-14 21:38:44 +0000

    added floop

---
+++ floop.py
@@ -0,0 +1,33 @@
+#! /usr/bin/python3
+
+print('what cake would you like to check first?\n')
+
+i=int(input("cake number: "))
+
+print(f'the cake number {i} is a lie\n')
+
+i+=1
+
+print(f'the cake number {i} is a lie\n')
+
+i+=1
+
+print(f'the cake number {i} is a lie\n')
+
+i+=1
+
+print(f'the cake number {i} is a lie\n')
+
+i+=1
+
+print(f'the cake number {i} is a lie\n')
+
+i+=1
+
+print(f'the cake number {i} is a lie\n')
+
+i+=1
+
+print(f'the cake number {i} is a lie\n')
+
+exit()
