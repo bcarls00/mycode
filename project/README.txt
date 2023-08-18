b608f5c0e93a3dcb00344461ccbbf74c9df43ab1
bcarls00 <bacarlson2200@gmail.com>
2023-08-18 15:43:27 -0700

    Fri eod

--- project/README.txt
+++ project/README.txt
@@ -1,14 +1,14 @@
 Welcome to Brian's Switch summarizer and configuration builder. 
 
 For getting a summarized, one page snapshot of your switch you will use the summarize.py script.
-This script will take the the current configuration file form the switch and output a summary to either a custom 
+This script will take the current configuration file form the switch and output a summary to either a custom 
 file or as a dated entry in the default switch_list file.
 First you want to copy your file to the project/sources directory.
 Then run the summarize.py script. Answer a few questions and you'll be on your way!
 
 
 For creating a new configuration file for a switch you will use the create_config.py script.
-This script will take as a standardized, easy to read file and output a file that when ran in the switch will 
+This script will take a standardized, easy to read file and output a file that when ran in the switch will 
 fully configure it to the specified parameters.
 First you need to copy the master.conf to the sources directory and edit it to your desired parameters.
 Then you will run the create_config.py script which will output the config file to the finished directory.
--- project/create_config.py
+++ project/create_config.py
@@ -5,11 +5,53 @@ import shutils
 
 def main():
     with open("/home/student/mycode/project/sources/master.conf", "r") as source:
+
         os.system("touch /home/student/mycode/project/finished/config")
+
         with open("/home/student/mycode/project/finished/config", "a") as config:
+            print("en\nconf t", file=config)
+
+#create and assign vlans
+
             for line in source:
-                if "fast" in source.lower:
-                    print(
+                intf = line.split(" ")
+                if "vlan" in source.lower and intf[2] is True:
+                    print(f"interface vlan {intf[2]}")
+                    if intf[3] is True:
+                        print(f"ip address {intf[3]} {intf[4]}", file=config)
+                    print("exit", file=config)
+
+#assign ports fa
+
+            for line in source:
+                if "fa" in source.lower:
+                    intf = line.split(" ")
+                    print(f"interface {intf[0]} {intf[1]}\n", file=config)
+                    if intf[3].lower == "yes":
+                        print(f"no shutdown", file=config)
+                    else:
+                        print("shutdown", file=config)
+                    
+                    if intf[4] is True:
+                        print(f"swithcport mode access\nswitchport access vlan {intf[4]}", file=config)
+                    
+                    print("exit", file=config)
+                
+#assign ports gi
+                if "gi" in source.lower:
+                    intf = line.split(" ")
+                    print(f"interface {intf[0]} {intf[1]}\n", file=config)
+                    if intf[3].lower == "yes":
+                        print(f"no shutdown", file=config)
+                    else:
+                        print("shutdown", file=config)
+                    
+                    if intf[4] is True:
+                        print(f"swithcport mode access\nswitchport access vlan {intf[4]}", file=config)
+                    
+                    print("exit", file=config)
+
+
 
 
 
